fa5d906d7e0ef653f651d98b4f6a9d186982d581
sambunarayan <soyou30257@gmail.com>
2021-04-05 21:55:28 +0900

    20210405 Jakarta ee
    ・Servlet Filter

---
+++ JBatchTest/src/main/java/jp/co/jeu/jbatch/ServletLogFilter.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.jeu.jbatch;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author soyou
+ */
+public class ServletLogFilter implements Filter {
+
+    private Logger logger = LogManager.getLogger(this.getClass().getSimpleName());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws IOException, ServletException {
+        String uri = ((HttpServletRequest) request).getRequestURI().toString();
+        long start = System.currentTimeMillis();
+        logger.info(uri + " started.");
+        fc.doFilter(request, response);
+        logger.info(uri + " end. " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
