b47980a0ead426df7c87ecbeac12f29aa3318d2a
sambunarayan <soyou30257@gmail.com>
2021-04-16 23:06:00 +0900

    20210416 TimoutTest
    ・TimeoutTest

---
+++ JBatchTest/src/main/java/jp/co/jeu/jbatch/resources/TimeoutResource.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.jeu.jbatch.resources;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author soyou
+ */
+@Path("/timeout")
+@RequestScoped
+public class TimeoutResource {
+
+    @Path("/exec")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response process() {
+
+        return Response.ok()
+                .build();
+    }
+}
